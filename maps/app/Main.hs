4b73ee8ce55fa50e0f595e75404a0af58fad0bc4
Wei Ning <wei@canva.com>
2019-07-14 14:51:57 +1000

    wip

--- maps/app/Main.hs
+++ maps/app/Main.hs
@@ -3,6 +3,8 @@ import qualified Data.Map.Strict as M
 import System.Environment
 import Control.Monad
 
+import Text.Regex.PCRE
+
 hLines :: Handle -> IO [String]
 hLines h = do
   isEOF <- hIsEOF h
@@ -11,9 +13,13 @@ hLines h = do
   else
     (:) <$> hGetLine h <*> hLines h
 
-main :: IO ()
-main = do
+parseLog :: IO ()
+parseLog = do
   h <- openFile "ca_dev_bastion_syslog.txt" ReadMode
   lines <- hLines h
   print $ length lines
   hClose h
+
+main :: IO ()
+main = do
+  print (getAllTextMatches $ "there is a cow 1337" =~ "(\\w+)" :: [String])
