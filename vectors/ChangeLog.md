b57d2de936a12cbbc90e9f68e5eae61acda1aed8
Wei Ning <wei@canva.com>
2019-07-14 15:53:59 +1000

    wip

---
+++ vectors/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for vectors
+
+## Unreleased changes
---
+++ vectors/app/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Data.Vector ((//), (!), (!?))
+
+main :: IO ()
+main = do
+  print 1
