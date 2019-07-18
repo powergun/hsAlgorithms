773c434f2f8ea21636201603510753d9aba5dd9b
Wei Ning <wei@canva.com>
2019-07-19 07:59:38 +1000

    wip

--- scripts/algorithms/folding/count_lines.hs
+++ scripts/algorithms/folding/count_lines.hs
@@ -1,4 +1,16 @@
 #!/usr/bin/env stack runghc
+ 
+-- source
+-- https://wiki.haskell.org/Simple_Unix_tools
+
+-- Count number of characters in a file (like wc -c)
+-- wc_c    = showln . length
+ 
+-- Count number of lines in a file, like wc -l
+-- wc_l    = showln . length . lines
+
+-- Count number of words in a file (like wc -w)
+-- wc_w    = showln . length . words
 
 main :: IO ()
 main = do
