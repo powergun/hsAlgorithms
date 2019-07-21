45872ab654a34964aa3388d35d5e36765d7ad8de
Wei Ning <wei@canva.com>
2019-07-21 16:48:08 +1000

    wip

--- scripts/containers/list/Helpers.hs
+++ scripts/containers/list/Helpers.hs
@@ -1,18 +1,31 @@
 module Helpers
   ( verify
+  , verify1
+  , assert
   , forM_
   ) where
 
 import           Control.Monad (forM_)
 
-verify :: ([Integer] -> Maybe Integer) -> [([Integer], Maybe Integer)] -> IO ()
+assert :: (Show a, Eq a) => a -> a -> IO ()
+assert lhs rhs =
+  case (lhs == rhs) of
+    True  -> return ()
+    False -> error $ "fail: " ++ show lhs ++ " != " ++ show rhs
+
+verify :: ([Integer] -> Maybe Integer)
+       -> [([Integer], Maybe Integer)]
+       -> IO ()
 verify f condidates = do
   forM_ condidates $ \condidate -> do
-    let input = fst condidate
+    let sequence = fst condidate
         expected = snd condidate
-    assert expected (f input)
-  where
-    assert lhs rhs =
-      case (lhs == rhs) of
-        True  -> return ()
-        False -> error $ "fail: " ++ show lhs ++ " != " ++ show rhs
+    assert expected (f sequence)
+
+verify1 :: ([Integer] -> Int -> Maybe Integer)
+        -> [([Integer], Int, Maybe Integer)]
+        -> IO ()
+verify1 f condidates = do
+  forM_ condidates $ \condidate -> do
+    let (sequence, index, expected) = condidate
+    assert expected (f sequence index)
--- scripts/containers/list/kth_elem.hs
+++ scripts/containers/list/kth_elem.hs
@@ -1,7 +1,27 @@
 #!/usr/bin/env stack runghc
 
-import           Helpers (forM_, verify)
+-- source
+-- https://wiki.haskell.org/99_questions/Solutions/3
+
+import           Helpers (forM_, verify1)
+
+kth1 [] _ = Nothing
+kth1 xs i | i < 0 = Nothing
+          -- won't work with infinite list!
+          | i >= length xs = Nothing
+          | otherwise = Just (xs !! (i - 1))
 
 main :: IO ()
 main = do
-  print 1
+  let condidates = [ ([], 1, Nothing)
+                   , ([1], -1, Nothing)
+                   , ([1], 23, Nothing)
+                   , ([1..9], 5, Just 5)
+                   , ([1..101], 100, Just 100)
+
+                   -- length xs will cause this to freeze
+                   -- , ([1..], 100, Just 100)
+                   ]
+  forM_ [ kth1
+        ] $ \f ->
+    verify1 f condidates
