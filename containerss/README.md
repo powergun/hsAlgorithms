d3ea91a3b6c55e392197ebb960011de752982dab
Wei Ning <macgnw@gmail.com>
2020-01-31 10:18:27 +1100

    vector

--- containerss/README.md
+++ containerss/README.md
@@ -12,3 +12,13 @@ First Principles P/1133
 > "unboxed" represents raw values without pointer indirection
 > "Slicing" refers to slicing off a portion, or creating a sub-array.
 > The Vector type is designed for making slicing much cheaper than it otherwise would be.
+
+Vector slicing is performant:
+
+> What makes Vector nicer than lists and Array in this respect is that
+> when you construct a slice or "view" of another Vector, it doesn’t
+> have to construct as much new data.
+> It returns a new wrapper around the original underlying array with a
+> new index and offset with a reference to the same original Array.
+
+Recall the immutable-ref idiom in Rust.
--- containerss/src/FirstPrinciples/Vector.hs
+++ containerss/src/FirstPrinciples/Vector.hs
@@ -1,7 +1,48 @@
-module FirstPrinciples.Vector (sliceNaive) where
+module FirstPrinciples.Vector (profSlice, profFusion) where
 
-import qualified Data.Vector as V
+import           Criterion.Main (bench, defaultMain, whnf)
+import qualified Data.Vector    as V
 
 sliceNaive :: Int -> Int -> [a] -> [a]
 sliceNaive from len xs =
   take len (drop from xs)
+
+l :: [Int]
+l = [1..1000]
+
+v :: V.Vector Int
+v = V.fromList [1..1000]
+
+-- fusion:
+-- First Principles, P/1137
+
+testV' :: Int -> V.Vector Int
+testV' n = V.map (+n)
+         $ V.map (+n)
+         $ V.map (+n)
+         $ V.map (+n)
+         (V.fromList [1..10000])
+
+testV :: Int -> V.Vector Int
+testV n = V.map
+          ( (+n)
+          . (+n)
+          . (+n)
+          . (+n)
+          ) (V.fromList [1..10000])
+
+profSlice :: IO ()
+profSlice = defaultMain
+  [ bench "//// slicing list" $
+    whnf (head . sliceNaive 100 900) l
+  , bench "//// slicing vector" $
+    whnf (V.head . V.slice 100 900) v
+  ]
+
+profFusion :: IO ()
+profFusion = defaultMain
+  [ bench "//// vector map prefused" $
+    whnf testV 9998
+  , bench "//// vector map will be fused" $
+    whnf testV' 9998
+  ]
