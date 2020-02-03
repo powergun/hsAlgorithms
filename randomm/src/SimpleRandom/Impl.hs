14cc7237184b1a3259a10474be8ee920dc825461
Wei Ning <macgnw@gmail.com>
2020-02-04 10:31:40 +1100

    random numbers generation

--- randomm/src/SimpleRandom/Impl.hs
+++ randomm/src/SimpleRandom/Impl.hs
@@ -4,8 +4,21 @@ module SimpleRandom.Impl (demo) where
 
 import qualified System.Random as Sr
 
-demo :: IO ()
-demo = do
+demoGenList :: Int -> IO ()
+demoGenList n = do
+  let gen :: a -> [(Int, Sr.StdGen)] -> [(Int, Sr.StdGen)]
+      gen _ xs = (: xs) . Sr.next . snd . head $ xs
+  g <- Sr.getStdGen
+  print $ fmap fst $ foldr gen [Sr.next g] [1..n]
+
+demoNext :: IO ()
+demoNext = do
   let g = Sr.mkStdGen 21
   let (num, g') = Sr.next g
   print num
+
+demo :: IO ()
+demo = do
+  print "//// Random number demo"
+  demoNext
+  demoGenList 10
