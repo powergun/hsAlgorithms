6fe32f81da86757f7b8bc6268ba0b4fce945b323
Wei Ning <nwcgnw@gmail.com>
2020-02-04 22:01:45 +1100

    leftist

--- leftistQ/README.md
+++ leftistQ/README.md
@@ -9,3 +9,13 @@
 > Heaps are often implemented as heap-ordered trees, in which the element
 > at each node is no larger than the elements at its children. Under this
 > ordering, the minimum element in a tree is always at the root.
+
+### Right Spine and Property
+
+> Note that the elements along the right spine of a leftist heap (in
+> fact, along any path through a heap-ordered tree) are stored in
+> sorted order.
+> The key insight behind leftist heaps is that two heaps can be merged
+> by merging their right spines as you would merge two sorted lists, and
+> then swapping the children of nodes along this path as necessary to
+> restore the leftist property.
--- leftistQ/src/PurelyFDS/Leftist.hs
+++ leftistQ/src/PurelyFDS/Leftist.hs
@@ -1,9 +1,16 @@
 module PurelyFDS.Leftist where
+import           Data.Bool                      ( bool )
+import           Prelude                 hiding ( elem )
+data Heap a = Leaf | Node {rank :: Int, elem :: a, l :: Heap a, r :: Heap a}
+              deriving (Eq, Ord, Show)
 
-data Heap a = Leaf
-            | Node { rank :: Int
-                   , elem :: a
-                   , l    :: Heap a
-                   , r    :: Heap a
-                   }
-            deriving (Eq, Ord, Show)
+mergeT :: (Eq a, Ord a) => Heap a -> Heap a -> Heap a
+mergeT Leaf n    = n
+mergeT n    Leaf = n
+mergeT n1   n2   = bool doLeft doRight (elem n1 <= elem n2)
+   where
+      doLeft  = makeT (elem n1) (l n1) (mergeT (r n1) n2)
+      doRight = makeT (elem n2) (l n2) (mergeT n1 (r n2))
+      rank' Leaf           = 0
+      rank' (Node r _ _ _) = r
+      makeT x n1 n2 = undefined
