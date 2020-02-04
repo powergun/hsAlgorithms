3ffc3de1765fcad212581cc487e14d7e60f52db0
Wei Ning <macgnw@gmail.com>
2020-02-04 20:49:39 +1100

    leftist

--- leftistQ/src/PurelyFDS/Leftist.hs
+++ leftistQ/src/PurelyFDS/Leftist.hs
@@ -1,2 +1,9 @@
 module PurelyFDS.Leftist where
 
+data Heap a = Leaf
+            | Node { rank :: Int
+                   , elem :: a
+                   , l    :: Heap a
+                   , r    :: Heap a
+                   }
+            deriving (Eq, Ord, Show)
--- leftistQ/test/LeftistSpec.hs
+++ leftistQ/test/LeftistSpec.hs
@@ -2,6 +2,8 @@ module LeftistSpec (spec) where
 
 import           Test.Hspec
 
+import qualified PurelyFDS.Leftist as Leftist
+
 spec = hspec $ do
   describe "" $ do
     it "" $ do
